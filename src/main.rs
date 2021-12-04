e3360667fce8a93b583f420078b4d563a6c3f94d
Eric Anopolsky <erpo41@gmail.com>
2021-12-03 21:07:17 -0700

    solves day 3 part 2

--- src/main.rs
+++ src/main.rs
@@ -58,6 +58,7 @@ fn main() {
         (2, 1) => solve_day_2_part_1(puzzle_input),
         (2, 2) => solve_day_2_part_2(puzzle_input),
         (3, 1) => solve_day_3_part_1(puzzle_input),
+        (3, 2) => solve_day_3_part_2(puzzle_input),
         _ => panic!(
             "The solution for day {} part {} is not implemented",
             day, part
@@ -228,4 +229,63 @@ fn solve_day_3_part_1(puzzle_input: String) -> u32 {
     gamma_rate /= 2;
     let epsilon_rate = 2_u32.pow(12) - 1 - gamma_rate;
     epsilon_rate * gamma_rate
+}
+
+fn solve_day_3_part_2(puzzle_input: String) -> u32 {
+    let diagnostic_report: Vec<&str> = puzzle_input
+        .split("\n")
+        .filter_map(|report_number: &str| -> Option<&str> {
+            if report_number == "" {
+                return None;
+            }
+            Some(report_number)
+        })
+        .collect();
+    let mut oxygen_generator_possibilities = diagnostic_report.clone();
+    for bit_position in 0..12 {
+        oxygen_generator_possibilities =
+            trim_diagnostic_report(oxygen_generator_possibilities, bit_position, true);
+        if oxygen_generator_possibilities.len() == 1 {
+            break;
+        }
+    }
+
+    let mut co2_scrubber_possibilities = diagnostic_report.clone();
+    for bit_position in 0..12 {
+        co2_scrubber_possibilities =
+            trim_diagnostic_report(co2_scrubber_possibilities, bit_position, false);
+        if co2_scrubber_possibilities.len() == 1 {
+            break;
+        }
+    }
+
+    u32::from_str_radix(oxygen_generator_possibilities[0], 2).unwrap()
+        * u32::from_str_radix(co2_scrubber_possibilities[0], 2).unwrap()
+}
+
+fn trim_diagnostic_report(
+    report: Vec<&str>,
+    bit_position: usize,
+    keep_most_common: bool,
+) -> Vec<&str> {
+    let report_length = report.len();
+    let mut one_count_at_bit_position = 0;
+    for report_element in report.iter() {
+        if report_element.chars().collect::<Vec<char>>()[bit_position] == '1' {
+            one_count_at_bit_position += 1;
+        }
+    }
+
+    let keep_ones: bool = (one_count_at_bit_position * 2 >= report_length && keep_most_common)
+        || (one_count_at_bit_position * 2 < report_length && !keep_most_common);
+
+    let mut trimmed_report: Vec<&str> = Vec::new();
+    for report_element in report.iter() {
+        let char_at_bit_position = report_element.chars().collect::<Vec<char>>()[bit_position];
+        if (char_at_bit_position == '1' && keep_ones) || (char_at_bit_position == '0' && !keep_ones)
+        {
+            trimmed_report.push(report_element);
+        }
+    }
+    trimmed_report
 }
