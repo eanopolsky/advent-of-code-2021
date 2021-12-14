df79f7b6be425613f696ec0093943c7097978c85
Eric Anopolsky <erpo41@gmail.com>
2021-12-13 19:39:32 -0700

    performance optimization: day 12 using vectors

--- src/day12.rs
+++ src/day12.rs
@@ -10,7 +10,7 @@ enum CaveSize {
 #[derive(Debug)]
 struct RoomProperties {
     size: CaveSize,
-    neighbors: HashSet<String>,
+    neighbors: Vec<usize>,
 }
 
 fn is_string_lower_case(test_string: &str) -> bool {
@@ -20,30 +20,28 @@ fn is_string_lower_case(test_string: &str) -> bool {
 }
 
 fn count_paths_to_end(
-    current_room: &str,
-    room_properties: &HashMap<String, RoomProperties>,
-    visit_counts: &HashMap<&str, u64>,
+    current_room_offset: &usize,
+    start_room_offset: &usize,
+    end_room_offset: &usize,
+    all_room_properties: &Vec<RoomProperties>,
+    visit_counts: &Vec<usize>,
     still_have_extra_time: bool,
 ) -> u64 {
-    // println!(
-    //     "Getting paths to end from {} with visit_counts = {:?} and still_have_extra_time = {}",
-    //     current_room, visit_counts, still_have_extra_time
-    // );
     let mut paths_to_end: u64 = 0;
     let mut updated_visit_counts = visit_counts.clone();
-    *updated_visit_counts.get_mut(current_room).unwrap() += 1;
-    for possible_next_room in room_properties.get(current_room).unwrap().neighbors.iter() {
-        if possible_next_room == "end" {
+    updated_visit_counts[*current_room_offset] += 1;
+    for possible_next_room_offset in all_room_properties[*current_room_offset].neighbors.iter() {
+        if possible_next_room_offset == end_room_offset {
             paths_to_end += 1;
             continue;
         }
-        if possible_next_room == "start" {
+        if possible_next_room_offset == start_room_offset {
             continue;
         }
 
         let possible_next_room_type: &CaveSize =
-            &room_properties.get(possible_next_room).unwrap().size;
-        let possible_next_room_visit_count = *visit_counts.get(&possible_next_room[..]).unwrap();
+            &all_room_properties[*possible_next_room_offset].size;
+        let possible_next_room_visit_count = visit_counts[*possible_next_room_offset];
         if *possible_next_room_type == CaveSize::Small
             && possible_next_room_visit_count > 0
             && !still_have_extra_time
@@ -51,8 +49,10 @@ fn count_paths_to_end(
             continue;
         }
         let paths_from_possible_next_room = count_paths_to_end(
-            possible_next_room,
-            room_properties,
+            possible_next_room_offset,
+            start_room_offset,
+            end_room_offset,
+            all_room_properties,
             &updated_visit_counts,
             still_have_extra_time
                 && (*possible_next_room_type == CaveSize::Big
@@ -60,72 +60,71 @@ fn count_paths_to_end(
         );
         paths_to_end += paths_from_possible_next_room;
     }
-    // println!("Got paths to end from {}: {:?}", current_room, paths_to_end);
     paths_to_end
 }
 
-fn parse_room_properties(puzzle_input: String) -> HashMap<String, RoomProperties> {
-    let room_pair_iter = puzzle_input.trim().split("\n").map(|line| {
+fn parse_room_properties(puzzle_input: &str) -> (Vec<RoomProperties>, usize, usize) {
+    let mut room_label_set: HashSet<&str> = HashSet::new();
+    for room_label in puzzle_input.trim().split(&['\n', '-'][..]) {
+        room_label_set.insert(room_label);
+    }
+    let mut room_to_offset_map: HashMap<&str, usize> = HashMap::new();
+    let mut all_room_properties: Vec<RoomProperties> = Vec::new();
+    let mut offset: usize = 0;
+    for room_label in room_label_set {
+        room_to_offset_map.insert(room_label, offset);
+        all_room_properties.push(RoomProperties {
+            size: match is_string_lower_case(room_label) {
+                true => CaveSize::Small,
+                false => CaveSize::Big,
+            },
+            neighbors: Vec::new(),
+        });
+        offset += 1;
+    }
+
+    for room_pair in puzzle_input.trim().split("\n").map(|line| {
         let mut cave_iter = line.split("-");
         (cave_iter.next().unwrap(), cave_iter.next().unwrap())
-    });
-    let mut rooms: HashMap<String, RoomProperties> = HashMap::new();
-    for room_pair in room_pair_iter {
-        for (first_room, second_room) in [room_pair, (room_pair.1, room_pair.0)] {
-            match rooms.get_mut(first_room) {
-                None => {
-                    rooms.insert(
-                        String::from(first_room),
-                        RoomProperties {
-                            size: match is_string_lower_case(first_room) {
-                                true => CaveSize::Small,
-                                false => CaveSize::Big,
-                            },
-                            neighbors: {
-                                let mut neighbors: HashSet<String> = HashSet::new();
-                                neighbors.insert(String::from(second_room));
-                                neighbors
-                            },
-                        },
-                    );
-                }
-                Some(existing_room_properties) => {
-                    existing_room_properties
-                        .neighbors
-                        .insert(String::from(second_room));
-                }
-            }
-        }
+    }) {
+        all_room_properties[*room_to_offset_map.get(room_pair.0).unwrap()]
+            .neighbors
+            .push(*room_to_offset_map.get(room_pair.1).unwrap());
+        all_room_properties[*room_to_offset_map.get(room_pair.1).unwrap()]
+            .neighbors
+            .push(*room_to_offset_map.get(room_pair.0).unwrap());
     }
-    rooms
-}
-
-fn initialize_visit_counts(
-    room_properties: &HashMap<String, RoomProperties>,
-) -> HashMap<&str, u64> {
-    let mut visit_counts: HashMap<&str, u64> = HashMap::new();
-    for room_label in room_properties.keys() {
-        if room_label == "start" {
-            visit_counts.insert(room_label, 1);
-        } else {
-            visit_counts.insert(room_label, 0);
-        }
-    }
-    visit_counts
+    (
+        all_room_properties,
+        *room_to_offset_map.get("start").unwrap(),
+        *room_to_offset_map.get("end").unwrap(),
+    )
 }
 
 pub(crate) fn solve_part_1(puzzle_input: String) -> u64 {
-    let room_properties = parse_room_properties(puzzle_input);
-    // println!("Room properties: {:?}", room_properties);
-    let visit_counts = initialize_visit_counts(&room_properties);
-    count_paths_to_end("start", &room_properties, &visit_counts, false)
+    let (all_room_properties, start_offset, end_offset) = parse_room_properties(&puzzle_input);
+    let visit_counts = vec![0 as usize; all_room_properties.len()];
+    count_paths_to_end(
+        &start_offset,
+        &start_offset,
+        &end_offset,
+        &all_room_properties,
+        &visit_counts,
+        false,
+    )
 }
 
 pub(crate) fn solve_part_2(puzzle_input: String) -> u64 {
-    let room_properties = parse_room_properties(puzzle_input);
-    // println!("Room properties: {:?}", room_properties);
-    let visit_counts = initialize_visit_counts(&room_properties);
-    count_paths_to_end("start", &room_properties, &visit_counts, true)
+    let (all_room_properties, start_offset, end_offset) = parse_room_properties(&puzzle_input);
+    let visit_counts = vec![0 as usize; all_room_properties.len()];
+    count_paths_to_end(
+        &start_offset,
+        &start_offset,
+        &end_offset,
+        &all_room_properties,
+        &visit_counts,
+        true,
+    )
 }
 
 #[cfg(test)]
